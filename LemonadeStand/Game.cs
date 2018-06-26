e6f4d3bda58fa289a8d5227ab4298725cae01d84
loganmiller001 <loganmiller001@live.com>
2018-06-26 13:17:55 -0500

    Made starting game function with prompt explaining rules of the game.

--- LemonadeStand/Game.cs
+++ LemonadeStand/Game.cs
@@ -8,5 +8,18 @@ namespace LemonadeStand
 {
     class Game
     {
+        public void RunGame()
+        {
+            StartMenu();
+
+        }
+
+        public void StartMenu()
+        {
+            Console.WriteLine("You will be running a lemonade stand for seven days, attempting to make as much revenue as possible.");
+            Console.WriteLine("You will be able to purchase supplies such as lemons, sugar, ice, and cups before you start a new day and be able to set the price and amount of each you put into a single pitcher of lemonade.");
+            Console.WriteLine("Use these to attempt to turn a profit and try and make as much money in one week as possilbe!");
+            Console.ReadLine();
+        }
     }
 }
